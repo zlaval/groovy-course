f69654fcd2657578a6e85fa2f55bb25fad6363d6
zalan <zlaval@gmail.com>
2019-04-12 23:32:57 +0200

    init project

--- src/main/groovy/com/zlrx/groovy/Developer.groovy
+++ src/main/groovy/com/zlrx/groovy/Developer.groovy
@@ -32,6 +32,51 @@ class Developer {
 
     }
 
+    void pattern() {
+        def text = "Hi, I'm your computer"
+        def pattern = ~/your/
+        def finder = text =~ pattern
+        def matcher = text ==~ pattern
+        println finder.size()
+        println matcher
+    }
+
+    void range() {
+        Range r = 1..10
+        println r
+        Range s = 1..<10
+        println s
+    }
+
+    void lists() {
+        def nums = [1, 2, 3, 4, 5, 4, 7] as LinkedList
+        println nums
+        def newList = nums + [8, 2]
+        println newList
+        nums << 10
+        println nums
+        nums.pop()
+        println nums
+        def newerList = nums - 4
+        println newerList
+
+
+        println nums
+        println nums[0..<4]
+
+    }
+
+    void maps() {
+        def map = [:]
+        println map
+        def addressKey = "Address key"
+        def person = [first: "Joe", last: "Doe", email: "jdk@mail.com", (addressKey): "Bp"]
+        println person
+        println person.last
+        person.facebook = "joedoefb"
+        println person
+    }
+
     static void main(String[] args) {
         Developer d = new Developer()
         d.firstName = "Joe"
@@ -42,6 +87,16 @@ class Developer {
 
         d.work()
         d.decision(10)
+        def path = $/C:\groovy\file/$
+        println path
+        def multiline = '''hi this
+                        is multiline'''
+        println multiline
+
+        d.pattern()
+        d.range()
+        d.lists()
+        d.maps()
     }
 
 }
